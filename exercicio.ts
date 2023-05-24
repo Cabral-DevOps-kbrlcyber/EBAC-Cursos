050c3a49b6e25ff112c4d457ec2440b876ef02f5
Ronário Cabral <116685501+Dev-Cabral@users.noreply.github.com>
2023-05-23 21:38:09 -0400

    exercicio_ts
    exercicio_ts

---
+++ exercicio.ts
@@ -0,0 +1,9 @@
+function multiplicarNumeros (Na: number = 10, Nb: number =20) {
+    return Na * Nb;
+}
+
+const nome: string = 'Cabral';
+
+function saudacoes(nome: string) {
+    return (`Ola ${nome}`)
+}
